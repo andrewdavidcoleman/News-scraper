7a3681efc93c352488ade5a82e96291c07a92346
Andrew Coleman <Andrew@lawn-128-61-127-169.lawn.gatech.edu>
2017-09-15 12:57:56 -0400

    added routes, built Article and Note models

--- server.js
+++ server.js
@@ -38,45 +38,126 @@ db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
 
-// Making a request for BBC's "webdev" board. The page's HTML is passed as the callback's third argument
-request("http://www.outdoorgearlab.com/articles", function(error, response, html) {
-  if (error) {
-    console.log(error);
-  };
 
-  // First, tell the console what server.js is doing
-  console.log("\n***********************************\n" +
-              "Grabbing every article name, topic, and\n" +
-              "description from Outdoor Gear Labs's article board:" +
-              "\n***********************************\n");
+// Routes
+// ==================================================
+// ++++++GET request to scrape Outdoor Gear Lab website
+app.get("/scrape", function(req, res) {
+  request("http://www.outdoorgearlab.com/articles", function(error, response, html) {
+    if (error) {
+      console.log(error);
+    };
 
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(html);
+    // Load the HTML into cheerio and save it to a variable
+    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+    var $ = cheerio.load(html);
 
-  // An empty array to save the data that we'll scrape
-  var results = [];
+    // An empty array to save the data that we'll scrape
+    var result = {};
 
-  // With cheerio, find each div-tag with the "nw-c-top..." class
-  // (i: iterator. element: the current element)
-  $("div.tag_tile_card").each(function(i, element) {
+    // With cheerio, find each div-tag with the "nw-c-top..." class
+    // (i: iterator. element: the current element)
+    $("div.tag_tile_card").each(function(i, element) {
 
-    // Save the text of the element in a "title" variable
-    var title = $(element).find(".tag_tile_title").text();
-    var description = $(element).find(".tag_tile_description").text();
-    var topic = $(element).find(".tag_tile_type").text();
+      // Save the text of the element in a "title" variable
+      result.title = $(element).find(".tag_tile_title").text();
+      result.description = $(element).find(".tag_tile_description").text();
+      result.topic = $(element).find(".tag_tile_type").text();
+      result.link = "www.outdoorgearlab.com" + $(element).children("a").attr("href");
 
-    // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // then save the values for any "href" attributes that the child elements may have
+      // Using our Article model, create a new entry
+      // This effectively passes the result object to the entry (and the title and link)
+      var entry = new Article(result);
 
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: "      " + title,
-      topic: "      " + topic,
-      description: description
+      // Now, save that entry to the db
+      entry.save(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        // Or log the doc
+        else {
+          console.log(doc);
+        }
+      });
     });
-  });
 
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
+    // Tell the browser that we finished scraping the text
+    res.send("Scape complete");
+  });
+});
+
+
+// +++++++++GET request to grab all the articles we scraped from the database
+app.get("/articles", function(req, res) {
+  // Grab every doc in the Articles array
+  Article.find({}, function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Or send the doc to the browser as a json object
+    else {
+      res.json(doc);
+    }
+  });
+});
+
+// ++++++++GET request to grab a particular article by id
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  Article.findOne({ "_id": req.params.id })
+  // ..and populate all of the notes associated with it
+  .populate("note")
+  // now, execute our query
+  .exec(function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise, send the doc to the browser as a json object
+    else {
+      res.json(doc);
+    }
+  });
+});
+
+// +++++++++POST request to create a new note or replace an existing one
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  var newNote = new Note(req.body);
+
+  // And save the new note the db
+  newNote.save(function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise
+    else {
+      // Use the article id to find and update it's note
+      Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
+      // Execute the above query
+      .exec(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        else {
+          // Or send the document to the browser
+          res.send(doc);
+        }
+      });
+    }
+  });
+});
+
+
+
+
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
 });
